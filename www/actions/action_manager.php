591a88daa0dd5e8b745c02ba4221910ec8ac4b02
guillaume <clear>
2023-01-12 15:34:26 +0100

    rolep1

--- www/actions/action_manager.php
+++ www/actions/action_manager.php
@@ -5,3 +5,17 @@ $role_manager = 200;
 if ($user->role >= $role_manager) {
 
 }
+$role_user = 50;
+if ($user->role >= $role_user){
+
+}
+$role_ban = 0;
+if($user-> role >= $role_ban){
+
+}
+$role_admin = 1000;
+
+if ($user->role >= $role_admin) {
+
+}
+
